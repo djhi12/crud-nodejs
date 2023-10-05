4c1eb6e346153a3175eb6670597b1ab715cbdc0b
djhi12 <ido20002@byui.edu>
2023-10-05 21:44:32 +0800

    Initial commit

---
+++ routes/auth.js
@@ -0,0 +1,19 @@
+// routes/auth.js
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+
+router.post('/register', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new User({ username, password: hashedPassword });
+        await user.save();
+        res.status(201).send('User registered successfully.');
+    } catch (error) {
+        res.status(500).send('Error registering user.');
+    }
+});
+
+module.exports = router;
