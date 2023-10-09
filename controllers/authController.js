77d38105941efa95fa3b1d10d73e053968d82845
djhi12 <ido20002@byui.edu>
2023-10-09 14:15:16 +0800

    Initial commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,8 @@
-// routes/auth.js
-const express = require('express');
-const router = express.Router();
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
 
-router.post('/register', async (req, res) => {
+// User registration
+exports.registerUser = async (req, res) => {
     try {
         const { username, password } = req.body;
         const hashedPassword = await bcrypt.hash(password, 10);
@@ -12,8 +10,17 @@ router.post('/register', async (req, res) => {
         await user.save();
         res.status(201).json({ message: 'User registered successfully' });
     } catch (error) {
-        res.status(500).json({ error: 'Error registering user' });
+        res.status(500).json({ error: 'Internal server error' });
     }
-});
+};
 
-module.exports = router;
+// User login
+exports.loginUser = (req, res) => {
+    res.status(200).json({ message: 'Login successful', user: req.user });
+};
+
+// User logout
+exports.logoutUser = (req, res) => {
+    req.logout();
+    res.status(200).json({ message: 'Logout successful' });
+};
--- models/user.js
+++ models/user.js
@@ -1,9 +1,34 @@
-// models/user.js
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, unique: true, required: true },
-    password: { type: String, required: true },
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true, // Removes leading/trailing spaces
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+        lowercase: true, // Store emails in lowercase to avoid case sensitivity issues
+    },
+    // You can add more fields like name, profile picture, etc. based on your application's requirements.
+    // Example:
+    // name: {
+    //   type: String,
+    //   required: true,
+    //   trim: true,
+    // },
+    // profilePicture: {
+    //   type: String,
+    //   default: 'default.jpg', // You can set a default profile picture
+    // },
 });
 
 module.exports = mongoose.model('User', userSchema);
