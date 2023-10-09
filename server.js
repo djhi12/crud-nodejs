2cb561996851b5311c9f1ddd42f3663a5ed394d2
djhi12 <ido20002@byui.edu>
2023-10-09 13:58:00 +0800

    Initial commit

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-// server.js
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const passport = require('passport');
+const session = require('express-session');
+const cors = require('cors');
 const itemsRoute = require('./routes/items');
+const authRoutes = require('./routes/auth');
 const { swaggerDocs, swaggerUi } = require('./swagger');
-const validateItem = require('./utils/validation'); // Import the validation function
-const itemController = require('./controllers/itemController'); // Import the item controller
+const validateItem = require('./utils/validation');
+const Item = require('./models/item');
+const User = require('./models/user');
 
 dotenv.config();
 
@@ -15,7 +19,7 @@ const port = process.env.PORT || 3000;
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true, // Recommended for index support
+    useCreateIndex: true,
 });
 
 const db = mongoose.connection;
@@ -29,49 +33,67 @@ db.once('open', () => {
 });
 
 app.use(express.json());
+app.use(cors());
 
-// Use the items route
+// Passport Configuration (OAuth setup)
+app.use(session({ secret: 'your-secret-key', resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Passport OAuth Configuration (Replace with your OAuth strategy setup)
+// passport.use(yourOAuthStrategy);
+
+// Serialize and deserialize user (Replace with your User model logic)
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+        done(err, user);
+    });
+});
+
 app.use('/api/items', itemsRoute);
+app.use('/api/auth', authRoutes);
 
-// Serve Swagger documentation
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-// Root route
 app.get('/', (req, res) => {
     res.send('Hello, this is the root of your API.');
 });
 
-// Create a new item with data validation and controller
 app.post('/api/items', async (req, res) => {
-    // Validate the request data
-    if (!validateItem(req.body)) {
-        return res.status(400).json({ error: 'Invalid item data' });
-    }
-
     try {
+        // Validate the request data
+        if (!validateItem(req.body)) {
+            return res.status(400).json({ error: 'Invalid item data' });
+        }
+
         const newItem = new Item(req.body);
         await newItem.save();
         res.status(201).json(newItem);
     } catch (error) {
+        console.error(error);
         res.status(500).json({ error: 'Unable to create item' });
     }
 });
 
-// Update an item by ID with data validation and controller
 app.put('/api/items/:id', async (req, res) => {
-    // Validate the request data
-    if (!validateItem(req.body)) {
-        return res.status(400).json({ error: 'Invalid item data' });
-    }
-
-    const { id } = req.params;
     try {
+        // Validate the request data
+        if (!validateItem(req.body)) {
+            return res.status(400).json({ error: 'Invalid item data' });
+        }
+
+        const { id } = req.params;
         const updatedItem = await Item.findByIdAndUpdate(id, req.body, { new: true });
         if (!updatedItem) {
             return res.status(404).json({ error: 'Item not found' });
         }
         res.status(200).json(updatedItem);
     } catch (error) {
+        console.error(error);
         res.status(500).json({ error: 'Unable to update item' });
     }
 });
